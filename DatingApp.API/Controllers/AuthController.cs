660f013e84db266c19f07f3f5057d3d7beae657e
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-31 17:20:28 +0200

    Use DTOs to get data from requests body

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,22 +18,24 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        // Use a Dto to define the format of data passed to the method from the http call
+        // Need [FromBody] with Dtos tu tells the API where to search for data
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             // TODO: validate request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _repo.UserExists(username))
+            if(await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username is already taken");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
             // Call the repository Register method to hash password and create a new user in DB 
-            var createUser = await _repo.Register(userToCreate, password);
+            var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Dtos
+{
+    // Used 
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
