e97a31c591bf18444de61b4d799a84a3f6b4e9df
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-31 11:29:45 +0200

    Change request to be async Use async calls to db

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -19,21 +20,21 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values
-        // Use IActionResult to return a HTTP response
+        // Use Task<IActionResult> to return a HTTP response in asynchrone mode
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
             // get all values in Values table to a list
-            var values = _context.Values.ToList();
+            var values =  await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
             // get the first matching value or null
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
