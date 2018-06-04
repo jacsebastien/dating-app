6744ddb53284e4d33beda1bec644327ab5b3da6f
Sébastien Jacques <sebastien@X-Wing.local>
2018-06-04 09:29:26 +0200

    Connect login form with component

--- dating-app-front/src/app/nav/nav.component.ts
+++ dating-app-front/src/app/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+    model: any = {};
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    login() {
+        console.log(this.model);
+    }
 }
