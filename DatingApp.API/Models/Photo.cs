12600aadba39319dd1e29b90381ff3cdb96ad7e1
Sébastien Jacques <sebastien@X-Wing.local>
2018-06-05 14:46:34 +0200

    Use DB Cascading Deletion in place of Restrict Deletion

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -9,5 +9,10 @@ namespace DatingApp.API.Models
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
+
+        // With User keys in Photo Class, force EntityFramework to use Cascading deletion (If user is deleted, all photos are deleted) 
+        // in place of Restrict deletion (Delete user, keep photos and set UserId to null)
+        public User User { get; set; }
+        public int UserId { get; set; }
     }
 }
