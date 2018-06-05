1728be035513f98f16dda747b7dbd41937a97293
Sébastien Jacques <sebastien@X-Wing.local>
2018-06-05 14:04:38 +0200

    Add AuthGuard

--- dating-app-front/src/app/app-routing.module.ts
+++ dating-app-front/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
+    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+    { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
+    { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- dating-app-front/src/app/app.module.ts
+++ dating-app-front/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BsDropdownModule } from 'ngx-bootstrap';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AuthService } from './services/auth.service';
 import { AlertifyService } from './services/alertify.service';
+import { AuthGuard } from './guards/auth.guard';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -36,7 +38,7 @@ import { AppRoutingModule } from './app-routing.module';
     BsDropdownModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [AuthService, AlertifyService],
+  providers: [AuthService, AlertifyService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
