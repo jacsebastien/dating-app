7ae3ba83d2a978d838c32ff73bbf63ed76e1bfcf
Sébastien Jacques <sebastien@X-Wing.local>
2018-06-07 16:42:40 +0200

    Restructure members components hierarchy Create member-card component Member card styling

--- dating-app-front/src/app/members/member-list/member-list.component.ts
+++ dating-app-front/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../models/user.model';
-import { UserService } from '../services/user.service';
-import { AlertifyService } from '../services/alertify.service';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { AlertifyService } from '../../services/alertify.service';
 
 @Component({
     selector: 'app-member-list',
