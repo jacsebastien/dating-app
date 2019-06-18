f518c26c8f3a22b4b88ff2f51f3e631e8c30f2a4
Sébastien <jac.sebastien@gmail.com>
2019-06-18 16:01:08 +0200

    create API with Visual Studio, rename front folder

---
+++ Api/Api/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
